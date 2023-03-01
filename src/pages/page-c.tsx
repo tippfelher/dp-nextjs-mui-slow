9d9b4a407766568615e387cf9c50deadac36ffb5
tippfelher <49718037+tippfelher@users.noreply.github.com>
2023-03-02 00:12:17 +0100

    .

---
+++ src/pages/page-c.tsx
@@ -0,0 +1,29 @@
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import LinearProgress from '@mui/material/LinearProgress';
+import AppBar from '@mui/material/AppBar';
+import Alert from '@mui/material/Alert';
+import Badge from '@mui/material/Badge';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+
+export default function BPage() {
+  return (
+    <>
+    <Typography />
+    <Avatar />
+    <LinearProgress />
+    <AppBar />
+    <Alert />
+    <Badge />
+    <Box />
+    <Checkbox />
+    <Button />
+    <Container />
+    <Divider />
+    </>
+  );
+}
