71aba427bba3003ebb6796d478810e43e48f25ac
tippfelher <49718037+tippfelher@users.noreply.github.com>
2023-03-01 23:58:07 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
+  /*
+  modularizeImports: {
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
   },
+  */
 }
 
 module.exports = nextConfig
